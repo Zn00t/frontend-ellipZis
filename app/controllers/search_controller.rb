b9ef5a7c1804245265c4705ac5282628fe970760
Fabi <fabian.boehm99@yahoo.de>
2021-09-02 10:39:36 +0200

    started to add the REST-request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,13 @@ class SearchController < ApplicationController
 
   def get
     @query = params[:query]
+
+  end
+
+  def request_api(url)
+    response = Excon.get(url)
+    return nil if response.status != 200
+    JSON.parse(response.body)
   end
 
 end
