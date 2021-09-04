34a54ade3233b3358a35ce49676eb95b3a6a8b69
Fabi <fabian.boehm99@yahoo.de>
2021-09-04 20:24:25 +0200

    implemented stop result api

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -1,9 +1,11 @@
 class ResultController < ApplicationController
 
   def index
-    @results = 10
+    @noOfResults = 10
     @query = params[:id]
     @result = request_api("https://v5.vbb.transport.rest/stops/#{@query}")
+    @departures = request_api("https://v5.vbb.transport.rest/stops/#{@query}/departures")
+    @arrivals = request_api("https://v5.vbb.transport.rest/stops/#{@query}/arrivals")
   end
 
 
